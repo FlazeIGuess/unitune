d5e33db100efcc73311e8fda29a2566cdac22754
Flaze <61559082+FlazeIGuess@users.noreply.github.com>
2026-01-29 16:15:33 +0100

    Delete filter-logs.ps1

--- filter-logs.ps1
+++
@@ -1,4 +0,0 @@
-# PowerShell script to filter out noisy Android system logs
-# Usage: .\filter-logs.ps1
-
-adb logcat | Select-String -Pattern "(PowerHalMgrImpl|ScrollIdentify|DynamicFramerate|ViewRootImpl|AutofillManager|OplusViewDragTouchViewHelper)" -NotMatch
