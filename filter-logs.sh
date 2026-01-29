81b632851494fa2c1c8786b7874e0427628c6c0c
Flaze <61559082+FlazeIGuess@users.noreply.github.com>
2026-01-29 16:15:10 +0100

    Delete filter-logs.sh

--- filter-logs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Filter out noisy Android system logs
-# Usage: ./filter-logs.sh
-
-adb logcat | grep -v -E "(PowerHalMgrImpl|ScrollIdentify|DynamicFramerate|ViewRootImpl|AutofillManager|OplusViewDragTouchViewHelper)"
